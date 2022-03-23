6ad4e880a27c25db0841dd5bafbc3a0eb2994468
Doggo-Math <88623437+Doggo-Math@users.noreply.github.com>
2022-03-23 19:23:38 -0400

    Add files via upload
    A calculator that automatically runs the Collatz Conjecture on a chosen number.

---
+++ Collatz.py
@@ -0,0 +1,15 @@
+print('This is a Collatz conjecture calculator')
+while 1 != 0:
+    a = x = int(input('Type in an integer: '))
+    i=0
+    while x != 1:
+        mod = x % 2
+        if (mod == 0):
+            x = x / 2
+            i = i + 1
+        else:
+            x = x * 3 + 1
+            i = i + 1
+        print(x)
+    print (str(a) + ' reaches 1 after ' + str(i) + ' iterations')
+    print (' ')
